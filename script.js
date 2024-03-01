372284ca76927746c85c7911f1f5261e2a6cedd9
AngelCub <151705480+AngelCub@users.noreply.github.com>
2024-02-29 23:21:46 -0300

    "[TAREA] sensei"

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
+///INTENTANDO EL COMMIT 
+
 
 class Ninja {
-  constructor(nombre, salud = 100, velocidad = 3, fuerza = 3) {
-    this.nombre = nombre;
-    this.salud = salud;
-    this.velocidad = velocidad;
-    this.fuerza = fuerza;
+    constructor(nombre, salud) {
+      this.nombre = nombre;
+      this.salud = salud;
+      this.velocidad = 3;
+      this.fuerza = 3;
+    }
+  
+
+    sayName() {
+      console.log(`El nombre del ninja es ${this.nombre}`);
+    }
+  
+    showStats() {
+      console.log(
+        `Nombre: ${this.nombre}, Salud: ${this.salud}, Velocidad: ${this.velocidad}, Fuerza: ${this.fuerza}`
+      );
+    }
+  
+    drinkSake() {
+      this.salud += 10;
+    }
   }
+  
 
-  sayName() {
-    console.log(`El nombre del ninja es ${this.nombre}`);
+  class Sensei extends Ninja {
+    constructor(
+      nombre,
+      salud = 200,
+      velocidad = 10,
+      fuerza = 10,
+      sabiduria = 10
+    ) {
+      super(nombre, salud, velocidad, fuerza);
+      this.sabiduria = sabiduria;
+    }
+  
+
+    speakWisdom() {
+      console.log(
+        "La sabiduría del sensei dice: El camino hacia la victoria no es fácil pero es inevitable"
+      );
+      super.drinkSake();
+    }
   }
-
-  showStats() {
-    console.log(`Nombre: ${this.nombre}, Salud: ${this.salud}, Velocidad: ${this.velocidad}, Fuerza: ${this.fuerza}`);
-  }
-
-  drinkSake() {
-    this.salud += 10;
-    console.log(`${this.nombre} ha bebido sake y ha recuperado 10 puntos de salud.`);
-  }
-}
-
-
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.showStats();
-ninja1.drinkSake();
-ninja1.showStats();
+  
+  const superSensei = new Sensei("Master Splinter");
+  superSensei.speakWisdom();
+  superSensei.showStats();
+   
